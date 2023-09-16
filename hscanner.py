66bc1bef5b4d8aad9d046548aad33b80b39024dc
Mael <125591289+H0ag@users.noreply.github.com>
2023-09-16 17:35:23 +0200

    Update hscanner.py
    V.1.2 -- Hscanner is now able to scan open ports on hosts

--- hscanner.py
+++ hscanner.py
@@ -10,20 +10,19 @@ import time
 import argparse
 from rich.progress import track
 from rich import print as print2
+import socket
 
 
 def get_local_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        # Connexion à une adresse IP inexistante pour déterminer l'adresse IP locale
         s.connect(('10.255.255.255', 1))
         ip = s.getsockname()[0]
         
-        # Obtenir les informations sur l'interface réseau associée à l'adresse IP
         interface = ni.gateways()['default'][ni.AF_INET][1]
         masque_cidr = ni.ifaddresses(interface)[ni.AF_INET][0]['netmask']
     except:
-        ip = '127.0.0.1'  # Utilisez une valeur par défaut en cas d'échec
+        ip = '127.0.0.1'
     finally:
         s.close()
     return ip, masque_cidr
@@ -39,7 +38,8 @@ def convert_decimal_to_binary(ip):
         ipConverted.append(binaire)
     
     return ''.join(ipConverted)
-    
+
+
 
 def generate_ips(ipv4, mask):
     ipv4 = convert_decimal_to_binary(ipv4)
@@ -58,29 +58,54 @@ def generate_ips(ipv4, mask):
     print(f"Network ip ({colored('Binary', attrs=['bold'])}):", colored("".join(etlogique), "red", attrs=['bold']))
     print(f"Network ip ({colored('Decimal', attrs=['bold'])}):", colored(socket.inet_ntoa(bytes(int("".join(etlogique)[i:i+8], 2) for i in range(0, 32, 8))), "red", attrs=['bold']))
     print("Available ip:",res)
-    print("="*50)
+    print(colored('='*50, "grey", attrs=['bold']))
 
-    # Adresse IP du réseau en décimal
     network_ip_decimal = str(socket.inet_ntoa(bytes(int("".join(etlogique)[i:i+8], 2) for i in range(0, 32, 8))))
 
-    # Nombre d'adresses IP disponibles
     num_available_ips = res
 
-    # Convertir l'adresse IP du réseau en une liste d'octets
     network_ip_octets = [int(octet) for octet in network_ip_decimal.split('.')]
 
     arrayresult.append("127.0.0.1")
 
-    # Générer toutes les adresses IP possibles dans la plage
     for i in range(num_available_ips):
-        # Ajouter l'indice actuel à chaque octet de l'adresse IP du réseau
         generated_ip_octets = [network_ip_octets[j] + (i >> (24 - j * 8) & 255) for j in range(4)]
         
-        # Convertir les octets générés en format décimal
         generated_ip_decimal = ".".join(map(str, generated_ip_octets))
         
         arrayresult.append(generated_ip_decimal)
 
+def ping_ip(ip, timeout):
+    param = '-n' if platform.system().lower() == 'windows' else '-c'
+    timeout = str(timeout)
+    command = ['ping', param, '1', '-W', timeout, ip]
+
+    if platform.system().lower() == 'windows':
+        output_file = 'NUL'
+    else:
+        output_file = '/dev/null'
+
+    p = subprocess.call(command, stdout=open(output_file, 'w'), stderr=open(output_file, 'w'))
+
+    return p
+
+def main(ip, timeout):
+    p = ping_ip(ip, timeout)
+
+    if(p == 0):
+        getHostName = socket.gethostbyaddr(ip)[0]
+        output = (
+            '[bold green][+][/bold green]'
+            f'[bold white] {str(ip)} [/bold white]'
+            ' Host is up'
+            f'[yellow] {str(getHostName)}[/yellow]'
+        )
+        print2(output, '[bold bright_black] (YOU)[/bold bright_black]' if str(ip) == str(iphost) else '')
+        res = "up"
+    else:
+        res = "down"
+    
+    return res
 
 def ipScanner(host, value, timeout):
     global arrayresult
@@ -98,34 +123,19 @@ def ipScanner(host, value, timeout):
 
     try:
         for ip in track(arrayresult, description="Processing"):
-            param = '-n' if platform.system().lower() == 'windows' else '-c'
-            timeout = str(timeout)
-            command = ['ping', param, '1', '-W', timeout, ip]
-
-            # Rediriger la sortie vers /dev/null ou NUL
-            if platform.system().lower() == 'windows':
-                output_file = 'NUL'
-            else:
-                output_file = '/dev/null'
-
-            p = subprocess.call(command, stdout=open(output_file, 'w'), stderr=open(output_file, 'w'))
-            
-            if(p == 0):
-                getHostName = socket.gethostbyaddr(ip)[0]
-                output = (
-                    '[bold green][+][/bold green]'
-                    f'[bold white] {str(ip)} [/bold white]'
-                    ' Host is up'
-                    f'[yellow] {str(getHostName)}[/yellow]'
-                )
-                #print(colored("[+]", "green", attrs=['bold']), colored(ip, attrs=['bold']), " Host is up", colored(getHostName, "yellow", attrs=['dark', "bold"]), colored("(YOU)", "grey", attrs=['bold']) if str(ip) == str(iphost) else '')
-                print2(output, '[bold bright_black] (YOU)[/bold bright_black]' if str(ip) == str(iphost) else '')
-                up = up+1
-            else:
-                #print(colored("[-]", "red", attrs=['bold']), colored(ip, attrs=['bold']), " Host is down")
-                down = down+1
-            
+            func = main(ip, timeout)
             total = total+1
+            if(func == "up"):
+                up = up+1
+                a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                for i in range(65535):
+                    location = (ip, i)
+                    result_of_check = a_socket.connect_ex(location)
+                    if result_of_check == 0:
+                        print2(f"[bright_black]↪ Port {i} is open.[/bright_black]")
+                a_socket.close()
+            elif(func == "down"):
+                down = down+1
 
     except KeyboardInterrupt:
         print("\nScript interrupted by user.")
@@ -136,8 +146,8 @@ if "__main__" == __name__:
     print("""██╗  ██╗███████╗ ██████╗ █████╗ ███╗   ██╗███╗   ██╗███████╗██████╗ \n██║  ██║██╔════╝██╔════╝██╔══██╗████╗  ██║████╗  ██║██╔════╝██╔══██╗\n███████║███████╗██║     ███████║██╔██╗ ██║██╔██╗ ██║█████╗  ██████╔╝\n██╔══██║╚════██║██║     ██╔══██║██║╚██╗██║██║╚██╗██║██╔══╝  ██╔══██╗\n██║  ██║███████║╚██████╗██║  ██║██║ ╚████║██║ ╚████║███████╗██║  ██║\n╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝
 """)
     print(colored("Made by Hoag.", "green", attrs=['bold']))
-    print(colored("Version: 1.1", "yellow", attrs=['bold']))
-    print("="*50)
+    print(colored("Version: 1.2", "yellow", attrs=['bold']))
+    print(colored('='*50, "grey", attrs=['bold']))
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', type=str, default=0.2, help='Time-out in seconds per ip tested. (Default: 0.2)')
@@ -148,7 +158,7 @@ if "__main__" == __name__:
     print(f"IpV4 ({colored('Decimal', attrs=['bold'])}): ",colored(iphost, "green", attrs=['bold']))
     print(f"Subnet mask ({colored('Binary', attrs=['bold'])}): ",colored(convert_decimal_to_binary(mask), "yellow", attrs=['bold']))
     print(f"Subnet mask ({colored('Decimal', attrs=['bold'])}): ",colored(mask, "yellow", attrs=['bold']),"\n")
-    
+
     n255 = Counter(list(str(mask).replace(".", " ").split(" ")))['255']
     n0 = Counter(list(str(mask).replace(".", " ").split(" ")))['0']
     
@@ -159,7 +169,7 @@ if "__main__" == __name__:
     start_time = time.time()
     result = ipScanner(iphost, mask, args.t)
 
-    print('='*50)
+    print(colored('='*50, "grey", attrs=['bold']))
     print('RESULT :')
     print(colored(result[2], "cyan", attrs=['bold']), f"host{'s' if result[2] > 1 else ''} were tested in {colored(time.time() - start_time, 'white', attrs=['bold'])} seconds.")
     print(colored(result[0], "green", attrs=['bold']), f"host{'s' if result[0] > 1 else ''} {'are' if result[0] > 1 else 'is'} up.")
