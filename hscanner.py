d018c6bd85515c112400e72e4470521b31f0eb6f
Mael <125591289+H0ag@users.noreply.github.com>
2023-09-23 16:40:54 +0200

    Update hscanner.py
    V1.2.1 -- 
    - Change the text in the loading bar to "Scanning" instead of "processing".
    - Added the time the host has taken to respond
    - Added the ability to scan open ports on a precise machine with the "--ip <IP>" argument.
    - If the "ping" command doesn't work, Hscanner suggests you install it (ON LINUX ONLY).

--- hscanner.py
+++ hscanner.py
@@ -11,6 +11,9 @@ import argparse
 from rich.progress import track
 from rich import print as print2
 import socket
+import ipaddress
+from sys import platform as platform2
+import os
 
 
 def get_local_ip():
@@ -38,7 +41,6 @@ def convert_decimal_to_binary(ip):
         ipConverted.append(binaire)
     
     return ''.join(ipConverted)
-
 
 
 def generate_ips(ipv4, mask):
@@ -85,18 +87,21 @@ def ping_ip(ip, timeout):
     else:
         output_file = '/dev/null'
 
+    time_counter = time.time()
     p = subprocess.call(command, stdout=open(output_file, 'w'), stderr=open(output_file, 'w'))
+    time_counter = time.time() - time_counter
 
-    return p
+    return p, time_counter
 
 def main(ip, timeout):
-    p = ping_ip(ip, timeout)
+    p, timec = ping_ip(ip, timeout)
 
     if(p == 0):
         try:
             getHostName = socket.gethostbyaddr(ip)[0]
             output = (
                 '[bold green][+][/bold green]'
+                f'[italic green] [{timec:.3f}s][/italic green]'
                 f'[bold white] {str(ip)} [/bold white]'
                 ' Host is up'
                 f'[yellow] {str(getHostName)}[/yellow]'
@@ -112,7 +117,7 @@ def main(ip, timeout):
         res = "up"
     else:
         res = "down"
-    
+
     return res
 
 def ipScanner(host, value, timeout):
@@ -130,21 +135,55 @@ def ipScanner(host, value, timeout):
     prog_bar = len(arrayresult)
 
     try:
-        for ip in track(arrayresult, description="Processing"):
-            func = main(ip, timeout)
-            total = total+1
-            if(func == "up"):
-                up = up+1
-                if args.p:
-                    a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    for i in range(65535):
-                        location = (ip, i)
-                        result_of_check = a_socket.connect_ex(location)
-                        if result_of_check == 0:
-                            print2(f"[bright_black]↪ Port {i} is open.[/bright_black]")
+        if(not args.ip):
+            for ip in track(arrayresult, description="Scanning..."):
+                func = main(ip, timeout)
+                total = total+1
+                if(func == "up"):
+                    up = up+1
+                    if args.p:
+                        a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        # a_socket.settimeout(0.1)
+                        for i in range(65535):
+                            location = (ip, i)
+                            try:
+                                result_of_check = a_socket.connect_ex(location)
+                            
+                                if result_of_check == 0:
+                                    print2(f"[bright_black]↪ Port {i} is open.[/bright_black]")
+                            except socket.error as e:
+                                print(f"Connection failed :/ : {e}")
+                                a_socket.close()
+                            
+                        a_socket.close()
+                elif(func == "down"):
+                    down = down+1
+        else:
+            a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            a_socket.settimeout(2)
+            for i in track(range(65535), description="Processing"):
+                location = (args.ip, i)
+                total = total + 1
+                try:
+                    result_of_check = a_socket.connect_ex(location)
+                
+                    if result_of_check == 0:
+                        output = (
+                            '[bold green][+][/bold green]'
+                            ' Port'
+                            f'[bold white] {int(i)} [/bold white]'
+                            'is opened on'
+                            f'[yellow] {str(args.ip)}[/yellow]'
+                        )
+                        print2(output)
+                        up = up + 1
+                    else:
+                        down = down + 1
+                except socket.error as e:
+                    print(f"Connection failed :/ : {e}")  
                     a_socket.close()
-            elif(func == "down"):
-                down = down+1
+                      
+            a_socket.close()
 
     except KeyboardInterrupt:
         print("\nScript interrupted by user.")
@@ -155,23 +194,37 @@ if "__main__" == __name__:
     print("""██╗  ██╗███████╗ ██████╗ █████╗ ███╗   ██╗███╗   ██╗███████╗██████╗ \n██║  ██║██╔════╝██╔════╝██╔══██╗████╗  ██║████╗  ██║██╔════╝██╔══██╗\n███████║███████╗██║     ███████║██╔██╗ ██║██╔██╗ ██║█████╗  ██████╔╝\n██╔══██║╚════██║██║     ██╔══██║██║╚██╗██║██║╚██╗██║██╔══╝  ██╔══██╗\n██║  ██║███████║╚██████╗██║  ██║██║ ╚████║██║ ╚████║███████╗██║  ██║\n╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝
 """)
     print(colored("Made by Hoag.", "green", attrs=['bold']))
-    print(colored("Version: 1.2", "yellow", attrs=['bold']))
+    print(colored("Version: 1.2.1", "yellow", attrs=['bold']))
     print(colored('='*50, "grey", attrs=['bold']))
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', type=str, default=0.2, help='Time-out in seconds per ip tested. (Default: 0.2)')
     parser.add_argument('-p', action='store_true', help='Scan open ports on hosts (BETA)')
+    parser.add_argument('--ip', type=str, help='Scan open ports of machine')
     args = parser.parse_args()
+    
+    if args.ip and not args.p:
+        parser.error("--ip requires -p")
+    elif args.ip and args.p:
+       args.ip = str(ipaddress.ip_address(args.ip))
 
     print(colored("SELECTED ARGUMENTS:", attrs=['bold']))
     if args.t != 0.2:
         print(f"{colored('[+]', 'green', attrs=['bold'])} Custom timeout: {args.t}s")
     if args.p:
         print(f"{colored('[+]', 'green', attrs=['bold'])} Scan open ports on hosts")
-    if not args.p and args.t == 0.2:
+    if args.ip:
+        print(f"{colored('[+]', 'green', attrs=['bold'])} Scan open ports on {args.ip}")
+    if not args.p and args.t == 0.2 and not args.ip:
         print(f"{colored('[-]', 'red', attrs=['bold'])} NONE")
 
     print("\n")
+
+    try:
+        ping_ip("https://google.com", 0.2)
+    except:
+        if platform2 == "linux" or platform2 == "linux2":
+            exit(f"{colored('[-]', 'red', attrs=['bold'])} The ping command doesn't work. Try: {colored('sudo apt-get install -y iputils-ping', attrs=['bold'])}")
 
     iphost, mask = get_local_ip()
     print(f"IpV4 ({colored('Binary', attrs=['bold'])}): ",colored(convert_decimal_to_binary(iphost), "green", attrs=['bold']))
@@ -191,7 +244,7 @@ if "__main__" == __name__:
 
     print(colored('='*50, "grey", attrs=['bold']))
     print('RESULT :')
-    print(colored(result[2], "cyan", attrs=['bold']), f"host{'s' if result[2] > 1 else ''} were tested in {colored(time.time() - start_time, 'white', attrs=['bold'])} seconds.")
-    print(colored(result[0], "green", attrs=['bold']), f"host{'s' if result[0] > 1 else ''} {'are' if result[0] > 1 else 'is'} up.")
-    print(colored(result[1], "red", attrs=['bold']), f"host{'s' if result[1] > 1 else ''} {'are' if result[1] > 1 else 'is'} down.")
+    print(colored(result[2], "cyan", attrs=['bold']), f"{'hosts' if not args.p else 'ports'} were tested in {colored(time.time() - start_time, 'white', attrs=['bold'])} seconds.")
+    print(colored(result[0], "green", attrs=['bold']), f"{'hosts' if not args.p else 'ports'} {'are' if result[0] > 1 else 'is'} up.")
+    print(colored(result[1], "red", attrs=['bold']), f"{'hosts' if not args.p else 'ports'} {'are' if result[1] > 1 else 'is'} down.")
     exit()
